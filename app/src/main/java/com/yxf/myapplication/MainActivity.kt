e87c494ce1737a653bafb72f19a372943a6ce51e
yxf <1anxuf@gmail.com>
2021-08-30 14:15:47 +0800

    NONE

--- app/src/main/java/com/yxf/myapplication/MainActivity.kt
+++ app/src/main/java/com/yxf/myapplication/MainActivity.kt
@@ -2,11 +2,16 @@ package com.yxf.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yxf.myapplication.base.BaseBindingActivity
+import com.yxf.myapplication.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseBindingActivity<ActivityMainBinding>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    }
+
+    override fun init() {
+        TODO("Not yet implemented")
     }
 }
